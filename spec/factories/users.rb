3b1fb85bf2893001ab99b9d23d73860a478caaaa
gitmimigit <mimi0008forg@gmail.com>
2022-05-12 20:50:40 +0900

    UsersFactoryBotの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    nickname              {Faker::Name.initials(number: 2)}
+    first_name            {テスト}
+    last_name             {テスト}
+    first_kana            {テスト}
+    last_kana             {テスト}
+    birth_date            {Faker::Date.between}
   end
 end
