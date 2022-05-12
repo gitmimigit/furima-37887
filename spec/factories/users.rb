704fe0e6a6578b3e9c63ac3ef755612591b54968
gitmimigit <mimi0008forg@gmail.com>
2022-05-12 21:18:57 +0900

    FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     nickname              {Faker::Name.initials(number: 2)}
-    first_name            {テスト}
-    last_name             {テスト}
-    first_kana            {テスト}
-    last_kana             {テスト}
-    birth_date            {Faker::Date.between}
+    first_name            {"テスト"}
+    last_name             {"テスト"}
+    first_kana            {"テスト"}
+    last_kana             {"テスト"}
+    birth_date            {Faker::Date.in_date_period}
   end
 end
