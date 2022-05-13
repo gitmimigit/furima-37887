b290b7ede69ee760914817031553feb1f78af6cb
gitmimigit <mimi0008forg@gmail.com>
2022-05-13 19:08:06 +0900

    モデルのアソシエーションの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,13 @@ class Item < ApplicationRecord
   validates :source_id, presence: true
   validates :duration_id, presence: true
   validates :price, presence: true
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :source
+  belongs_to :duration
 end
