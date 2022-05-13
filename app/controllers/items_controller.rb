74ce267463729035d49451fc68190e4d61af43cc
gitmimigit <mimi0008forg@gmail.com>
2022-05-13 16:48:27 +0900

    ルーティング、コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,15 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item =Item.new
+  end
+
+  def create
+    Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :describe, :category_id, :status_id, :fee_id, :source_id, :duration_id, :price, :image).merge(user_id: current_user.id)
   end
 end
