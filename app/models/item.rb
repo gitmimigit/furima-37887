8bb3536e694850649a1a9940c24e50682bb9feb5
gitmimigit <mimi0008forg@gmail.com>
2022-05-13 18:38:19 +0900

    itemモデルのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :describe, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :fee_id, presence: true
+  validates :source_id, presence: true
+  validates :duration_id, presence: true
+  validates :price, presence: true
 end
