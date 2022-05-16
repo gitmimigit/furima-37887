372355a38ff7ae871a601dc586fcfda32d28f2b8
gitmimigit <mimi0008forg@gmail.com>
2022-05-16 16:07:24 +0900

    factoriesの記入

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name     {Faker::Lorem.word}
+    describe      {Faker::Lorem.sentence}
+    category_id   {1}
+    status_id     {1}
+    fee_id        {1}
+    source_id     {1}
+    duration_id   {1}
+    price         {Faker::Number.number(digits: 4)}
+    association :user 
   end
 end
