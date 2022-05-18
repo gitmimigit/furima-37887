87bf32ced227e42ca99a729b3f11b90bc9048668
gitmimigit <mimi0008forg@gmail.com>
2022-05-18 19:42:46 +0900

    orders/indexの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
   def index
+    @item = Item.find(params[:id])
     @order_delivery = OrderDeliverie.new
   end
 
