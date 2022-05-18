f3ee2a0a59eada783c4735af98754a9d4520d78e
gitmimigit <mimi0008forg@gmail.com>
2022-05-18 17:04:59 +0900

    orderコントローラーのindexアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  def index
+    @order_delivery = OrderDeliverie.new
+  end
+
+  def create
+  end
 end
