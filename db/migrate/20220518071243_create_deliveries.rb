ca99ac088f355890d7c8c1fa8330094f33a53ad5
gitmimigit <mimi0008forg@gmail.com>
2022-05-19 14:08:03 +0900

    マイグレーションファイルの記述

--- db/migrate/20220518071243_create_deliveries.rb
+++ db/migrate/20220518071243_create_deliveries.rb
@@ -1,6 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
+      t.string     :postal_code,   null: false
+      t.integer    :source_id,     null: false
+      t.string     :city,          null: false
+      t.string     :address_line1, null: false
+      t.string     :address_line2
+      t.integer    :number,     null: false
+      t.references :order,         null: false,foreign_key: true
 
       t.timestamps
     end
